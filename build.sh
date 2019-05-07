d42d91a742d0f808a73aed061f7d3e6ebf9e70a1
arturol76 <arturo.lotito@gmail.com>
2019-05-07 20:26:12 +0200

    first commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#example:
+#./build.sh 192.168.2.96
+
+IMAGE_NAME = arturol76/letsencrypt-dns
+
+echo building...
+docker -H $1 build -t $IMAGE_NAME .
+
+echo pushing...
+docker -H $1 push $IMAGE_NAME
