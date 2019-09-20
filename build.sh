14b6d116f8416aa18029378158b505c6e4dcd2b4
arturol76 <arturo.lotito@gmail.com>
2019-09-20 20:27:58 +0200

    now it asks before building and pushing

--- build.sh
+++ build.sh
@@ -3,8 +3,25 @@
 
 IMAGE_NAME=arturol76/letsencrypt-dns-ssh
 
-echo building...
-docker -H $1 build -t $IMAGE_NAME .
+read -p "Do you want to build image? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo
 
-echo pushing...
-docker -H $1 push $IMAGE_NAME
+    echo building image...
+    docker -H $1 build --no-cache -t $IMAGE_NAME .
+else
+    echo
+fi
+
+read -p "Do you want to push image to docker repository? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo
+
+    echo pushing...
+    docker -H $1 push $IMAGE_NAME
+else
+    echo
+fi
+
